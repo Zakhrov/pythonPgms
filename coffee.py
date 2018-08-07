819725bbd928204008e53cab0a2056ed75b3e5fc
Agila <agila@suse-aaron.suse>
2018-08-07 09:42:47 +0530

    initial commit

---
+++ coffee.py
@@ -0,0 +1,21 @@
+import urllib.request
+import time
+def get_price():
+    page=urllib.request.urlopen("file:///home/agila/myfirst.html")
+    decoding=page.read().decode("utf8")
+    where=decoding.find('>$')
+    startprice=where+2
+    endprice=startprice+4
+    return float(decoding[startprice:endprice])
+
+option=input("do you want the price immediatelt?y/n")
+if option=="y":
+    print(get_price())
+
+else:
+    price=99
+    while price>4.74:
+        get_price()
+        price=get_price()
+    print(price)
+    print("buy")
