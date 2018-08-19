f8072d8301084e4cb28f6aea15fdcf175568f7b1
agila <agila2612@gmail.com>
2018-08-19 14:49:56 +0530

    new commint

--- scorespgm.py
+++ scorespgm.py
@@ -2,4 +2,4 @@ line="101;johnny waveboy' Jones;USA;8:32;Fish;21"
 s={}
 s['id'],s['name'],s['country'],s['average'],s['board'],s['age']=line.split(";")
 print("id " + s['id'])
-
+#hi this is a comment
